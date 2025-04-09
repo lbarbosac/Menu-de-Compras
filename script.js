c3de80c3a9e263c954d68adf447ba97e9f58e7ee
Lucas Barbosa <lucas2304barbosa@gmail.com>
2025-04-09 18:57:34 -0300

    .

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const contador = document.querySelector('.quantidade');
+const buttonDeAdd = document.querySelector('.adicionar-button');
+const buttonDeRemove = document.querySelector('.remover-button');
+
+buttonDeAdd.addEventListener('click', () => {
+    let value = parseInt(contador.value);
+    value += 1;
+    contador.value = value;
+});
+
+buttonDeRemove.addEventListener('click', () => {
+    let value = parseInt(contador.value);
+    if (value >0){
+        value -= 1;
+        contador.value = value;
+    }
+});
